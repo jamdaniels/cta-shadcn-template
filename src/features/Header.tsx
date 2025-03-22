affa7c867680ec709f7c854d64115b22c2eb0813
jamdaniels <cemayatac@proton.me>
2025-03-22 03:15:11 +0100

    same as before

--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -1,16 +1,22 @@
+import { Link } from "@tanstack/react-router";
 import { Button } from "./ui/button";
+import { Banana } from "lucide-react";
 
 export function Header() {
   return (
     <div className="flex justify-between items-center p-4">
       <div>
-        <Button variant="link" className="font-bold">
-          Logo
-        </Button>
+        <Link to="/">
+          <Button variant="link" className="font-bold">
+            <Banana className="size-6" /> Bananas
+          </Button>
+        </Link>
       </div>
       <div className="flex gap-2">
         <Button variant="outline">Sign up</Button>
-        <Button>Login</Button>
+        <Link to="/login">
+          <Button>Login</Button>
+        </Link>
       </div>
     </div>
   );
